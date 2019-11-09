d60bacdac85a5995bb2c1f00c8905affd2a8e169
akash2237778 <akash2237778@gmail.com>
2019-11-09 15:12:38 +0530

    ec2 termination added

--- aws_cli_json_parse.py
+++ aws_cli_json_parse.py
@@ -2,11 +2,33 @@
 
 import subprocess as sb
 i=0
+
+
 sb.getoutput("touch describe-instances_file")
 sb.getoutput("aws ec2 describe-instances > describe-instances_file ")
+
+
 instance_id = "akash"
-instance_id_array = []
+instance_id_list = []
+instance_state = "akash"
+instance_state_list = []
+
+
 while  not instance_id ==  "null":
 	instance_id = sb.getoutput("cat describe-instances_file | jq '.Reservations["+str(i)+"].Instances[0].InstanceId'")
 	i = i+1
-	print(instance_id)
+	if not instance_id == "null" :
+		instance_id_list.append(instance_id)
+
+
+i=0
+while  not instance_state ==  "null":
+	instance_state = sb.getoutput("cat describe-instances_file | jq '.Reservations["+str(i)+"].Instances[0].State.Name'")
+	i = i+1
+	if not instance_state == "null" :
+		instance_state_list.append(instance_state)
+
+i = 0
+for id_ in instance_id_list:
+	print( id_ + ":" +  instance_state_list[instance_id_list.index(id_)])
+	i = i+1
