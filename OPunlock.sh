2da351ab2dfaa2dbe8572f33062c6bbc38b8ef8b
akash2237778 <akash2237778@gmail.com>
2019-11-09 14:15:16 +0530

    ec2 playbooks created 9 nov 2019

--- OPunlock.sh
+++ OPunlock.sh
@@ -2,7 +2,7 @@
 
 # adb tcpip 5555
  
-adb connect 25.57.77.230
+adb connect 25.79.227.245
 
  adb shell input keyevent 82 && adb shell input swipe 200 900 200 300 && adb shell input text 78569 && adb shell input keyevent 66
 
